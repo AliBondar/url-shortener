3de6e3d38717263ee9d27158b9360c2f290098b9
Ali Bondar <bondarali1380@gmail.com>
2025-10-15 20:44:33 +0330

    feat: add /version endpoint

--- src/main/java/com/bondar/urlshortener/controller/UrlController.java
+++ src/main/java/com/bondar/urlshortener/controller/UrlController.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -16,7 +18,7 @@ public class UrlController {
 
     private final UrlService urlService;
 
-    @PostMapping("/api/shorten")
+    @PostMapping("/shorten")
     public ResponseEntity<Map<String, String>> shortenUrl(@RequestBody Map<String, String> body) {
         String originalUrl = body.get("url");
         ShortUrl shortUrl = urlService.shortenUrl(originalUrl);
@@ -25,12 +27,22 @@ public class UrlController {
         return ResponseEntity.ok(Map.of("shortUrl", baseUrl + shortUrl.getShortCode()));
     }
 
-    @GetMapping("/s/{shortCode}")
+    @GetMapping("/{shortCode}")
     public RedirectView redirectToOriginal(@PathVariable String shortCode) {
         String originalUrl = urlService.getOriginalUrl(shortCode);
         RedirectView redirectView = new RedirectView();
         redirectView.setUrl(originalUrl);
         return redirectView;
+    }
+
+    @GetMapping("/version")
+    public ResponseEntity<Map<String, String>> getVersion() {
+        Map<String, String> versionInfo = new HashMap<>();
+        versionInfo.put("version", "1.0.1");
+        versionInfo.put("description", "URL Shortener Service");
+        versionInfo.put("author", "Ali Bondar");
+        versionInfo.put("date", LocalDateTime.now().toString());
+        return ResponseEntity.ok(versionInfo);
     }
 
 }
